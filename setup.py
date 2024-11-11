8d109074f363805758e049741b1e855405676cb4
spynom <saurav24082000@gmail.com>
2024-11-11 12:00:09 +0530

    project completed

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'QApplication',
+    version= '0.0.1',
+    author= 'saurav kumar',
+    author_email= 'spynom01@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
